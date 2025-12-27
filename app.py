915b6fb306e625e1f9883a4629a30b970388a7c2
Marlo André Camargo <marlocamargo978@gmail.com>
2025-12-27 04:26:56 -0300

    Alteração de codigo

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 
 st.set_page_config(page_title="Relatorio de vendas", layout="wide")
+st.title("Dashboard de Vendas")
 
 #Tentativa de carregamento dos dados
 try:
@@ -10,13 +11,16 @@ try:
     
     #Mostrando a tabela
     st.subheader("Visão geral dos dados")
-    st.dataframe(df.head())
-    st.subheader("Vendas por categoria/produto")
-    
-     #Cria um grafico simples se houver colunas numericas
-    st.bar_chart(df.select_dtypes(include=['float', 'int']))
-    
-    
+    st.dataframe(df)
 
+    st.subheader("Vendas por dia da semana")
+    coluna_texto = "Dia"
+
+    if coluna_texto in df.columns:
+        dados_grafico = df.set_index(coluna_texto)
+
+    else:
+        st.error(f"Não encontrei a coluna '{coluna_texto}' no CSV.")
+    
 except FileNotFoundError:
     st.error("Erro: O arquivo 'vendas.csv' nao foi encontrado na pasta")
